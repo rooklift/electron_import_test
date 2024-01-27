7b398da74061e4d8194c5b487d0e615604655357
rooklift <16438795+rooklift@users.noreply.github.com>
2024-01-27 22:57:22 +0000

    Initial

---
+++ modules/start.js
@@ -0,0 +1,5 @@
+"use strict";
+
+import { message } from "./message.js";
+
+document.getElementById("foo").innerHTML = message;
---
+++ renderer.js
@@ -0,0 +1,3 @@
+"use strict";
+
+import "./modules/start.js"		// Import style used for side-effects only
